1412b0ad8cb04953e9f7e2c85b5020f3d42444be
marti <mart2004xd@gmail.com>
2025-03-07 15:19:56 +0200

    .

--- notes.js
+++ notes.js
@@ -1,30 +1,30 @@
 //? Github info
 
-//! Basic info
-//* M : Modified file
-//* A : Added a new file
-//* D : Deleted file
-//* U : Untracked file
+    //! Basic info
+        //* M : Modified file
+        //* A : Added a new file
+        //* D : Deleted file
+        //* U : Untracked file
 
-//* Merging is when you transfer the features to main/master from another branch
+        //* Merging is when you transfer the features to main/master from another branch
 
-//! Commands for git bash
-//* git branch new_branch --> make a new branch
-//* git switch new_branch --> switch to new branch
-//* git checkout new_branch --> switch to new branch
-//* git switch main
-//* git merge new_branch
-//* git branch -d branch_name --> delete branch
-//* git branch -D branch_name --> force delete (before merge)
+    //! Commands for git bash
+        //* git branch new_branch --> make a new branch
+        //* git switch new_branch --> switch to new branch
+        //* git checkout new_branch --> switch to new branch
+        //* git switch main
+        //* git merge new_branch
+        //* git branch -d branch_name --> delete branch
+        //* git branch -D branch_name --> force delete (before merge)
 
 //? VS Code info
-//! Shortcuts
-//* Ctrl + Shift + E --> opens Explorer
-//* Ctrl + Shift + P --> opens Command Palette
-//* Ctrl + Shift + G --> opens Source Control
+    //! Shortcuts
+        //* Ctrl + Shift + E --> opens Explorer
+        //* Ctrl + Shift + P --> opens Command Palette
+        //* Ctrl + Shift + G --> opens Source Control
 
-//* Ctrl + ~ --> opens / focuses Terminal
-//* Ctrl + Shift + ~ --> opens a new instance of Terminal
+        //* Ctrl + ~ --> opens / focuses Terminal
+        //* Ctrl + Shift + ~ --> opens a new instance of Terminal
 
-//* Ctrl + B --> hides sidebar
-//* Alt + Z--> toggles text wrapping
+        //* Ctrl + B --> hides sidebar
+        //* Alt + Z--> toggles text wrapping
