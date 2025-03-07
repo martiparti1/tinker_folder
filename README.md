818132b1f37e5116e6de1860ed5dbbfa9340d0d0
marti <mart2004xd@gmail.com>
2025-03-07 14:51:38 +0200

    added github refreshers on the notes file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # tinker_folder
+
 random stuff i am tinkering with
+
+notes.js right now is like a little notepad for the whole directory
+(using .js because code formating ¯\_(ツ)\_/¯)
---
+++ notes.js
@@ -0,0 +1,24 @@
+// * --> this is a highlight
+// ! --> this is a warning
+// ? --> this is an idea / suggestion
+// TODO --> this is a TODO reminder
+
+//! Github info
+
+//* M : Modified file
+//* A : Added a new file
+//* D : Deleted file
+//* U : Untracked file
+
+//* git branch new_branch --> make a new branch
+//* git switch new_branch --> switch to new branch
+//* git checkout new_branch --> switch to new branch
+
+//? Merging
+
+//* Merging is when you transfer the features to main/master from another branch
+//* git switch main
+//* git merge new_branch
+
+//* git branch -d branch_name --> delete branch
+//* git branch -D branch_name --> force delete (before merge)
