1fe2813c92f1d5c08a3c37606c676067dc03b265
marti <mart2004xd@gmail.com>
2025-03-07 15:15:27 +0200

    notes changes

--- notes.js
+++ notes.js
@@ -1,24 +1,30 @@
-// * --> this is a highlight
-// ! --> this is a warning
-// ? --> this is an idea / suggestion
-// TODO --> this is a TODO reminder
+//? Github info
 
-//! Github info
-
+//! Basic info
 //* M : Modified file
 //* A : Added a new file
 //* D : Deleted file
 //* U : Untracked file
 
+//* Merging is when you transfer the features to main/master from another branch
+
+//! Commands for git bash
 //* git branch new_branch --> make a new branch
 //* git switch new_branch --> switch to new branch
 //* git checkout new_branch --> switch to new branch
-
-//? Merging
-
-//* Merging is when you transfer the features to main/master from another branch
 //* git switch main
 //* git merge new_branch
-
 //* git branch -d branch_name --> delete branch
 //* git branch -D branch_name --> force delete (before merge)
+
+//? VS Code info
+//! Shortcuts
+//* Ctrl + Shift + E --> opens Explorer
+//* Ctrl + Shift + P --> opens Command Palette
+//* Ctrl + Shift + G --> opens Source Control
+
+//* Ctrl + ~ --> opens / focuses Terminal
+//* Ctrl + Shift + ~ --> opens a new instance of Terminal
+
+//* Ctrl + B --> hides sidebar
+//* Alt + Z--> toggles text wrapping
